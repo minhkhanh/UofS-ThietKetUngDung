55da80af4ddc965d24e7df1ac4805702c1c6a8c4
akhoi90@gmail.com <akhoi90@gmail.com@20a004b0-139d-f758-6745-9c67f3e55304>
2011-03-26 17:11:20 +0000

    Commit xac nhan files.

---
+++ PQ/PQ/PQ/Character.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System.Xml;
+
+namespace PQ
+{
+    public class Character : GameEntity
+    {
+        public override GameObject Clone()
+        {
+            Character entity = new Character();
+
+            for (int i = 0; i < _mainSprites.Count; ++i)
+                entity._mainSprites.Add(new Sprite2D(_mainSprites[i], -1, -1));
+            
+            entity._x = this._x;
+            entity._y = this._y;
+
+            return entity;
+        }
+    }
+}
---
+++ PQ/PQ/PQ/Map.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PQ
+{
+    public class Map : GameEntity
+    {
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            foreach (Sprite2D i in _mainSprites)
+                if (i.Bound.Intersects(spriteBatch.GraphicsDevice.PresentationParameters.Bounds))
+                    i.Draw(gameTime, spriteBatch);
+        }
+    }
+}
