65830811f0a9aa218b15049fdc6a1c4b4518f916
minhkhanhhaugiang@gmail.com <minhkhanhhaugiang@gmail.com@20a004b0-139d-f758-6745-9c67f3e55304>
2011-06-21 17:14:05 +0000

    map

---
+++ PQ/PQ/PQ/GameStateExplorer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PQ
+{
+    public class GameStateExplorer : GameState
+    {
+        public override GameStateId StateId
+        {
+            get
+            {
+                return GameStateId.StateExplorer;
+            }
+        }
+        public GameStateExplorer(MyGame game)
+            : base(game)
+        {
+
+        }
+
+        private Map _globalMap;
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            _globalMap = (GlobalMap) this._game.MapManager.CreateObject(0);
+            this._gameObjects.Add(_globalMap);
+        }
+        
+    }
+}
